5a72da68c01dc3153f18ad9e2a07cb40f134d22b
Long Nguyen <192123324+LongNguyenUC@users.noreply.github.com>
2025-04-05 11:11:56 -0400

    Made button turn music on/off

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 let musicOn = true;
 const musicButton = document.querySelector(".music");
-let icon = musicButton.querySelector("i");
+const icon = musicButton.querySelector("i");
+const backgroundNoise = document.querySelector("#backgroundNoise");
 
 musicButton.addEventListener("click",(e)=>{
     
     if(musicOn){
         icon.classList.remove("fa-volume-high");
         icon.classList.add("fa-volume-xmark");
+        backgroundNoise.muted = true;
+
     }else{
         icon.classList.remove("fa-volume-xmark");
         icon.classList.add("fa-volume-high");
+        backgroundNoise.muted = false;
     }
 
     musicOn = !musicOn;
